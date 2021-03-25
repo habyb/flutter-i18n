18844283b05f0639cc11aaf29bd4b344b743b307
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-25 17:15:41 -0300

    i18n framework

--- lib/components/localization.dart
+++ lib/components/localization.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutteri18n/components/error.dart';
+import 'package:flutteri18n/components/progress.dart';
 
 import 'container.dart';
 
@@ -33,4 +35,69 @@ class ViewI18N {
 
     return values[_language];
   }
+}
+
+@immutable
+abstract class I18NMessagesState {
+  const I18NMessagesState();
+}
+
+@immutable
+class LoadingI18NMessagesSatate extends I18NMessagesState {
+  const LoadingI18NMessagesSatate();
+}
+
+@immutable
+class InitI18NMessagesState extends I18NMessagesState {
+  const InitI18NMessagesState();
+}
+
+@immutable
+class LoadedI18NMessagesState extends I18NMessagesState {
+  final I18NMessages _messages;
+
+  const LoadedI18NMessagesState(this._messages);
+}
+
+class I18NMessages {
+  final Map<String, String> _messages;
+
+  I18NMessages(this._messages);
+
+  String get (String key) {
+    assert(key != null);
+    assert(_messages.containsKey(key));
+    return _messages[key];
+  }
+}
+
+@immutable
+class FatalErrorI18NMessagesState extends I18NMessagesState {
+  const FatalErrorI18NMessagesState();
+}
+
+class ContactsListCubit extends Cubit<I18NMessagesState> {
+  ContactsListCubit() : super(InitContactsListState());
+
+  void reload(ContactDao dao) async {
+    emit(LoadingContactsListState());
+    dao.findAll().then((contacts) => emit(LoadedContactsListState(contacts)));
+  }
+}
+
+class I18NLoadingView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<I18NMessagesCubit, I18NMessagesState>(
+      builder: (context, state) {
+        if(state is InitI18NMessagesState || state is LoadingI18NMessagesSatate){
+          return ProgressView();
+        }
+        if(state is LoadedI18NMessagesState){
+          return TELA;
+        }
+        return ErrorView('Erro buscando mensagens da tela');
+      }
+    ),
+  };
 }
