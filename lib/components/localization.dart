4f5678d5e042a73b8132fb8c03e37d23cda06348
Habyb Fernandes <habybfernandes@gmail.com>
2021-03-25 16:24:27 -0300

    care and variations

--- lib/components/localization.dart
+++ lib/components/localization.dart
@@ -28,6 +28,9 @@ class ViewI18N {
   }
 
   String localize(Map<String, String> values) {
+    assert(values != null);
+    assert(values.containsKey(_language));
+
     return values[_language];
   }
 }
